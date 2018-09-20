97e550b821daa3707401bde26e4a843707068faf
Erin Green <erin.y.green@gmail.com>
2018-09-20 17:58:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-   ingredients.include?("cheddar", "gouda", "camembert") do |ingredient|
+   ingredients.include?(cheese_types) do |ingredient|
      if ingredient == true 
        return ingredient
      else return nil 
