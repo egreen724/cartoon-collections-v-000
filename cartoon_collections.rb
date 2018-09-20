48854108f7a024f46e8cd2e106747d30cb0c84e7
Erin Green <erin.y.green@gmail.com>
2018-09-20 17:23:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,6 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves_names)
+  dwarves_names.each_with_index |name, index|
+  puts "#{index + 1} #{name}"
   # Your code here
 end
 
